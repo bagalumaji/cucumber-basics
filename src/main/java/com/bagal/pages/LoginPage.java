a3575d77ba1660c65a7f1e1e8405a7b1fdede5fa
bagalumaji <bagalumaji@gmail.com>
2023-11-29 10:26:08 -0800

    added locator for username, password and login button

--- src/main/java/com/bagal/pages/LoginPage.java
+++ src/main/java/com/bagal/pages/LoginPage.java
@@ -1,4 +1,12 @@
 package com.bagal.pages;
 
+import org.openqa.selenium.By;
+
 public class LoginPage {
+    private static final By TEXT_BOX_USER_NAME =By.id("uid");
+    private static final By TEXT_BOX_PASSWORD =By.id("passw");
+    private static final By BTN_LOGIN =By.name("btnSubmit");
+
+
+
 }
