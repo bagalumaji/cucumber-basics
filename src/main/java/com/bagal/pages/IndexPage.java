d2804238ba0a3aa4670d2ffa507e62452cc2fc52
bagalumaji <bagalumaji@gmail.com>
2023-11-29 10:25:38 -0800

    added login link locator and method

--- src/main/java/com/bagal/pages/IndexPage.java
+++ src/main/java/com/bagal/pages/IndexPage.java
@@ -1,4 +1,17 @@
 package com.bagal.pages;
 
-public class IndexPage {
+import com.bagal.pageactions.PageAction;
+import org.openqa.selenium.By;
+
+public class IndexPage extends BasePage {
+    private final static By LOGIN_LINK = By.id("LoginLink");
+
+    @Override
+    public boolean isPageLoaded() {
+        return PageAction.isDisplayed(LOGIN_LINK);
+    }
+
+    public void clickOnLoginLink(){
+        PageAction.waitForElementToBeVisibleAndClick(LOGIN_LINK);
+    }
 }
