c2923d42490a0fa0a6c4fee083bf538a7ba1a61f
bagalumaji <bagalumaji@gmail.com>
2023-11-29 15:04:33 -0800

    removed unused imports

--- src/main/java/com/bagal/factories/WaitFactory.java
+++ src/main/java/com/bagal/factories/WaitFactory.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.Objects;
-
 public final class WaitFactory {
     private WaitFactory() {
     }
