23b5987fffe363b29b549851c2f29cb3d70c7cf0
bagalumaji <bagalumaji@gmail.com>
2023-11-29 15:12:15 -0800

    optimized object creation

--- src/test/java/com/bagal/steps/login/LoginSteps.java
+++ src/test/java/com/bagal/steps/login/LoginSteps.java
@@ -8,33 +8,38 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.Assert;
 
+import static com.bagal.utils.PropertyUtil.getValue;
+
 public class LoginSteps {
+    private static final LoginPage LOGIN_PAGE = new LoginPage();
+    private static final IndexPage INDEX_PAGE = new IndexPage();
+
     @Given("user navigates to the website")
     public void userNavigatesToTheWebsite() {
-        boolean pageLoaded = new IndexPage().isPageLoaded();
+        boolean pageLoaded = INDEX_PAGE.isPageLoaded();
         Assert.assertTrue(pageLoaded);
 
     }
 
     @When("user click on login link")
     public void userClickOnLoginLink() {
-        LoginPage loginPage = new IndexPage().clickOnLoginLink();
+        LoginPage loginPage = INDEX_PAGE.clickOnLoginLink();
         Assert.assertTrue(loginPage.isPageLoaded());
     }
 
     @And("user enter username")
     public void userEnterUsername() {
-       new LoginPage().typeUserName("jsmith");
+        LOGIN_PAGE.typeUserName(getValue("username"));
     }
 
     @And("user enter password")
     public void userEnterPassword() {
-        new LoginPage().typePassword("demo1234");
+        LOGIN_PAGE.typePassword(getValue("password"));
     }
 
     @And("user click on login button")
     public void userClickOnLoginButton() {
-       new LoginPage().clickOnLoginButton();
+        LOGIN_PAGE.clickOnLoginButton();
     }
 
     @Then("user landing on home page")
@@ -45,11 +50,11 @@ public class LoginSteps {
 
     @And("user enter {string} username")
     public void userEnterUsername(String username) {
-        new LoginPage().typeUserName(username);
+        LOGIN_PAGE.typeUserName(username);
     }
 
     @And("user enter {string} password")
     public void userEnterPassword(String password) {
-        new LoginPage().typePassword(password);
+        LOGIN_PAGE.typePassword(password);
     }
 }
