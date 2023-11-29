acb5893a23726ab3b72582862a741b76069d4d56
bagalumaji <bagalumaji@gmail.com>
2023-11-29 10:35:37 -0800

    updated method type username and type password

--- src/main/java/com/bagal/pages/LoginPage.java
+++ src/main/java/com/bagal/pages/LoginPage.java
@@ -3,10 +3,12 @@ package com.bagal.pages;
 import com.bagal.pageactions.PageAction;
 import org.openqa.selenium.By;
 
+import static com.bagal.pageactions.PageAction.waitForElementToBeVisibleAndType;
+
 public class LoginPage extends BasePage {
-    private static final By TEXT_BOX_USER_NAME =By.id("uid");
-    private static final By TEXT_BOX_PASSWORD =By.id("passw");
-    private static final By BTN_LOGIN =By.name("btnSubmit");
+    private static final By TEXT_BOX_USER_NAME = By.id("uid");
+    private static final By TEXT_BOX_PASSWORD = By.id("passw");
+    private static final By BTN_LOGIN = By.name("btnSubmit");
 
 
     @Override
@@ -14,20 +16,22 @@ public class LoginPage extends BasePage {
         return PageAction.isDisplayed(TEXT_BOX_USER_NAME);
     }
 
-    public void performLoginToApplication(String userName, String password){
-
+    public HomePage performLoginToApplication(String userName, String password) {
+        typeUserName(userName).typePassword(password).clickOnLoginButton();
+        return new HomePage();
     }
 
-    private LoginPage typeUserName(String userName){
-
-        return this;
-    }
-    private LoginPage typePassword(String password){
-
+    private LoginPage typeUserName(String userName) {
+        waitForElementToBeVisibleAndType(TEXT_BOX_USER_NAME, userName);
         return this;
     }
 
-    private void clickOnLoginButton(){
+    private LoginPage typePassword(String password) {
+        waitForElementToBeVisibleAndType(TEXT_BOX_PASSWORD, password);
+        return this;
+    }
+
+    private void clickOnLoginButton() {
         PageAction.waitForElementToBeVisibleAndClick(BTN_LOGIN);
     }
 }
