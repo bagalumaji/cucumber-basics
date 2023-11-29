6179e4362390c72992127fddf48459ef8bf6b0ba
bagalumaji <bagalumaji@gmail.com>
2023-11-29 10:27:09 -0800

    added methods for waitForElementToBeVisibleAndClick, isDisplayed

---
+++ src/main/java/com/bagal/pageactions/PageAction.java
@@ -0,0 +1,32 @@
+package com.bagal.pageactions;
+
+import com.bagal.factories.WaitFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+
+public final class PageAction {
+    private PageAction(){}
+
+    public static void waitForElementToBeVisibleAndClick(By locator){
+        WaitFactory.waitForElementToBeVisible(locator).click();
+    }
+    public static void waitForElementToBeVisibleAndClick(By locator,int timeout){
+        WaitFactory.waitForElementToBeVisible(locator,timeout).click();
+    }
+
+    public static boolean isDisplayed(By locator){
+        try{
+            return WaitFactory.waitForElementToBeVisible(locator).isDisplayed();
+        }catch (TimeoutException e){
+            return false;
+        }
+    }
+
+    public static boolean isDisplayed(By locator,int timeout){
+        try{
+            return WaitFactory.waitForElementToBeVisible(locator,timeout).isDisplayed();
+        }catch (TimeoutException e){
+            return false;
+        }
+    }
+}
