55c9485c85d3bc66446ddeb140bddd5f3687fe83
bagalumaji <bagalumaji@gmail.com>
2023-11-29 15:04:03 -0800

    removed hard coded value to property

--- src/main/java/com/bagal/factories/DriverFactory.java
+++ src/main/java/com/bagal/factories/DriverFactory.java
@@ -5,13 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+import static com.bagal.utils.PropertyUtil.getValue;
+import static java.lang.Integer.parseInt;
+
 public final class DriverFactory {
     private DriverFactory(){}
 
     public static WebDriver getDriver(){
         WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(parseInt(getValue("pageloadtimeout"))));
         return driver;
     }
 }
