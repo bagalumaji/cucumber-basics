b4742603e813c17ceb6796391a565dc73abb7522
bagalumaji <bagalumaji@gmail.com>
2023-11-29 14:39:10 -0800

    added step for selenium

--- src/test/java/com/bagal/steps/login/LoginSteps.java
+++ src/test/java/com/bagal/steps/login/LoginSteps.java
@@ -1,49 +1,55 @@
 package com.bagal.steps.login;
 
+import com.bagal.pages.IndexPage;
+import com.bagal.pages.LoginPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 public class LoginSteps {
     @Given("user navigates to the website")
     public void userNavigatesToTheWebsite() {
-        //Driver.initDriver();
+        boolean pageLoaded = new IndexPage().isPageLoaded();
+        Assert.assertTrue(pageLoaded);
+
     }
 
     @When("user click on login link")
     public void userClickOnLoginLink() {
-        System.out.println("login link");
+        LoginPage loginPage = new IndexPage().clickOnLoginLink();
+        Assert.assertTrue(loginPage.isPageLoaded());
     }
 
     @And("user enter username")
     public void userEnterUsername() {
-        System.out.println("username");
+       new LoginPage().typeUserName("jsmith");
     }
 
     @And("user enter password")
     public void userEnterPassword() {
-        System.out.println("pass");
+        new LoginPage().typePassword("demo1234");
     }
 
     @And("user click on login button")
     public void userClickOnLoginButton() {
-        System.out.println("login button");
+       new LoginPage().clickOnLoginButton();
     }
 
     @Then("user landing on home page")
     public void userLandingOnHomePage() {
         System.out.println("home page");
-      //  Driver.quitDriver();
+
     }
 
     @And("user enter {string} username")
     public void userEnterUsername(String username) {
-        System.out.println("username = " + username);
+        new LoginPage().typeUserName(username);
     }
 
     @And("user enter {string} password")
     public void userEnterPassword(String password) {
-        System.out.println("password = " + password);
+        new LoginPage().typePassword(password);
     }
 }
