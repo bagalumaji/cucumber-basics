bea13b3e726eea34c9a793ca4ffbc3c08a05320f
bagalumaji <bagalumaji@gmail.com>
2023-11-29 14:58:11 -0800

    created property util read the property util

---
+++ src/main/java/com/bagal/exceptions/PropertyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bagal.exceptions;
+
+public class PropertyNotFoundException extends RuntimeException{
+    public PropertyNotFoundException(String message){
+        super(message);
+    }
+}
