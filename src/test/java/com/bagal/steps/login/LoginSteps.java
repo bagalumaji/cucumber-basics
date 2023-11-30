e96f3d3abab1785a340a063699e33fdb98151811
bagalumaji <bagalumaji@gmail.com>
2023-11-30 01:04:11 -0800

    removed unused import

--- src/test/java/com/bagal/steps/login/LoginSteps.java
+++ src/test/java/com/bagal/steps/login/LoginSteps.java
@@ -7,7 +7,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.testng.Assert;
 
 import static com.bagal.utils.PropertyUtil.getValue;
 import static org.testng.Assert.assertTrue;
