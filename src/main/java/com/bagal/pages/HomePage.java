506d9bc15c280e5e92848ebece8b12145e12973a
bagalumaji <bagalumaji@gmail.com>
2023-11-30 00:59:52 -0800

    added method

--- src/main/java/com/bagal/pages/HomePage.java
+++ src/main/java/com/bagal/pages/HomePage.java
@@ -1,4 +1,14 @@
 package com.bagal.pages;
 
-public class HomePage {
+import com.bagal.constants.HomePageConstants;
+import com.bagal.pageactions.PageAction;
+import com.bagal.utils.XpathUtil;
+import org.openqa.selenium.By;
+
+public class HomePage extends BasePage {
+    private static final String HELLO_USER_NAME_XPATH="//h1[normalize-space()='Hello %s']";
+    @Override
+    public boolean isPageLoaded() {
+        return PageAction.isDisplayed(By.xpath(XpathUtil.createXpath(HELLO_USER_NAME_XPATH,HomePageConstants.JOHN_SMITH)));
+    }
 }
